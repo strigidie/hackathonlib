ec24f35c3d292058bd3dd8bb06b5c37808f75b1a
strigidie <20969918+strigidie@users.noreply.github.com>
2025-09-06 15:45:49 -0700

    Update Routes

--- api/index.py
+++ api/index.py
@@ -1,11 +1,49 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+from flask import Flask, request
+from supabase import Client, create_client 
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Supabase key
+supabase_url: str = os.environ.get("SUPABASE_URL")
+supabase_key: str = os.environ.get("SUPABASE_KEY")
 
-@app.route('/about')
-def about():
-    return 'About'
+supabase: Client = create_client(supabase_url, supabase_key)
+
+# Main key of application
+app_key: str = os.environ.get("APP_KEY")
+app: Flask = Flask(__name__)
+
+@app.route('/api/create/profile', methods=["POST"])
+def create_profile():
+    data = request.get_json()
+
+    if not data:
+        return "No Data"
+
+    if not "key" in data:
+        return "No credentials"
+
+    if data["key"] != app_key:
+        return "Incorrect credentials"
+    
+    response = (
+        supabase.table("profiles")
+            .insert({
+                "name": "Johh",
+                "lastname": "Doe",
+                "age": 24,
+                "gender" : "M",
+                "height" : 178,
+                "weight" : 60
+
+            }).execute()
+    )
+
+    print(data)
+    return ""
+
+@app.route('/api/upload/image', methods=["POST"])
+def upload_image():
+    data = request.get_json()
+    print(data)
+    return ""
